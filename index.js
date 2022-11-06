93ae61295eca046a91931debfd1846a3a8b75fe2
Cristian Contreras <contreras.cristian994@gmail.com>
2022-11-06 13:04:45 -0500

    commit tree bff47578e5e886babf997ad60fb2ddefae51e627

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    var newCats = cats.slice();
+    newCats.push("Broom");
+    return newCats;
+}
+
+function prependCat() {
+    var newCats = cats.slice();
+    newCats.unshift("Arnold");
+    return newCats;
+}
+
+function removeLastCat() {
+    var Gatos = cats.slice();
+    Gatos.pop();
+    return Gatos;
+}
+function removeFirstCat() {
+    var Gotos = cats.slice();
+    Gotos.shift();
+    return Gotos;
+}
